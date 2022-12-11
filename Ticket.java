d758fee02302f31459813a58742bc7b9fc935a13
Dylan Coe <dtcoe@wisc.edu>
2022-12-11 04:01:10 +0000

    reverted Ticket class back to my own

--- Ticket.java
+++ Ticket.java
@@ -1,51 +1,36 @@
-// --== CS400 Project Two File Header ==--
-// Name: Gaurav Chopra
-// CSL Username: chopra
-// Email: gmchopra@wisc.edu
-// Lecture #: 002 @2:30pm
-// Notes to Grader: None
+// --== CS400 Project 3 File Header Information ==--
+// Name: Dylan Coe
+// CSL Username: dcoe
+// Email: dtcoe@wisc.edu
+// Lecture #: 002 @2:30
+// Notes to Grader:
+public class Ticket implements ITicket{
+  String departure;
+  String destination;
+  String price;
+  public Ticket(String departure, String destination, String price) {
+    this.departure = departure;
+    this.destination = destination;
+    this.price = price;
+  }
+  
+  @Override
+  public String getDeparture() {
+    return this.departure;
+  }
+  
+  @Override
+  public String getDestination() {
+    return this.destination;
+  }
+  @Override
+  public String getPrice() {
+    return this.price;
+  }
 
-/**
- * This class creates ticket objects that are used by the frontend developer
- */
-public class Ticket implements ITicket {
-    // Attributes of the ticket objects
-    String price;
-    String destination;
-    String departure;
-    String genre;
+  public String toString() {
+    return "[ Departure: " + this.getDeparture() + ", Destination: " + this.getDestination() + ", Price: " + this.getPrice() + " ]";
+  }
 
-    /**
-     * Constructor which creates the ticket objects
-     */
-    public Ticket(String price, String departure, String destination) {
-        this.price = price;
-        this.destination = destination;
-        this.departure = departure;
-    }
-
-    /**
-     * Getter method for the price of the ticket
-     */
-    @Override
-    public String getPrice() {
-        return this.price;
-    }
-
-    /**
-     * Getter method for the destination of the ticket
-     */
-    @Override
-    public String getDestination() {
-        return this.destination;
-    }
-
-    /**
-     * Getter method for the departure of the ticket
-     */
-    @Override
-    public String getDeparture() {
-        return this.departure;
-    }
 }
 
