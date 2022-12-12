4e6a50fdc71a9e056786a803fb19e70c41141e48
Yanbin Chen <ychen877@wisc.edu>
2022-12-12 02:07:43 +0000

    Added FlightMapper class

---
+++ FlightMapper.java
@@ -0,0 +1,26 @@
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Scanner;
+
+public class FlightMapper {
+    
+    public static void main(String[] args) throws FileNotFoundException {
+        ITicketLoader ticketLoader = new TicketLoader();
+	// load the flight tickets from the data file
+	List<ITicket> ticketList = ticketLoader.loadTickets(TicketDOT.gv);
+	// instantiate the back-end
+	ITicketBackend backend = new TicketBackend(new FlightTicketGraph());
+	// add all the tickets into the back-end
+	for (ITicket ticket: ticketList) {
+	    backend.addTicket(ticket);
+	}
+	// instantiate the scanner for user input
+	Scanner userInputScanner = new Scanner(System.in);
+        // instantiate the front-end and pass references to the scanner
+	// and back-end to it
+	ITicketFrontend frontend = new TicketFrontend(userInputScanner, backend);
+	// start the input loop of the front end
+	frontend.runCommandLoop();
+    }
+    
+}
